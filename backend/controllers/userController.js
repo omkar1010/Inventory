739377beac5c41ec73b7729d243c5144e3a48615
Omkar Jadhav <jadhavomkar1408@gmail.com>
2024-04-02 12:59:53 +0530

    create user controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,9 @@
+const registerUser = async (req,res) =>{
+    res.send("Register User")
+};
+
+module.exports = {
+    registerUser,
+    
+
+}
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const { registerUser } = require("../controllers/userController");
 
 const router = express.Router();
 
 
-const registerUser = () =>{};
+
 
 router.post("/register", registerUser)
 
