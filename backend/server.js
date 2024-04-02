8c9c5b4e415e58f8913609af8bb2b556fc0da379
Omkar Jadhav <jadhavomkar1408@gmail.com>
2024-04-02 11:42:45 +0530

    create Home route

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,18 @@ const cors = require("cors");
 
 const app = express()
 
+// middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: false })) // parse application/
+app.use(bodyParser.json());
+
+
+// Routes
+
+app.get("/" , (req, res)=>{
+    res.send("Home page");
+})
+
 const PORT = process.env.PORT  || 5000 ; 
 
 // connect to db and start server
